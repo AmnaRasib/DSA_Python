b97351204f64a7519fba4a85acad5ebd5074ac98
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 12:19:34 +0500

    Update task8.py

---
+++ LAB_1/task8.py
@@ -0,0 +1,8 @@
+fruits=["apple","Banana","cherry"]
+print("First fruit: ",fruits[0])
+#Modifying 
+fruits.append("orange")
+print("List after adding orange: ",fruits)
+# loop
+for fruit in fruits:
+    print("Fruit ",fruit)

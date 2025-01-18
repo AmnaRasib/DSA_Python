4fba39d588c5ab26e3ef4ac73de5b09f93df69f2
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 17:50:40 +0500

    Create task12.py

---
+++ LAB_1/task12.py
@@ -0,0 +1,12 @@
+# Python If-Else
+input= int(input("Enter a number: "))
+
+if input>0:
+    
+    print(input,"is positive integer")
+elif input<0:
+    
+    print(input,"is negative integer")
+else:
+    print("The number is zero.")
+

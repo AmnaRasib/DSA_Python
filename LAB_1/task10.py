b4f8ded476d65c001edf1117480cf4b1a1c783ae
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 17:50:29 +0500

    Create task10.py

---
+++ LAB_1/task10.py
@@ -0,0 +1,9 @@
+numbers={1,2,3,4}
+numbers.add(5)
+numbers.remove(2)
+print("Updated Set: ",numbers)
+
+even_numbers={2,4,6}
+print("Union= ",numbers.union(even_numbers))
+print("Intersection= ",numbers.intersection(even_numbers))
+

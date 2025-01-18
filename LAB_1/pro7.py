3b662dc607350e66bc4c382dcc18b5b6eda2dc0d
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 20:32:16 +0500

    Create pro7.py

---
+++ LAB_1/pro7.py
@@ -0,0 +1,4 @@
+text="Python Programming is so fun.Python Programming is powerful language.Python Programming is just like English Language."
+substring="Python Programming"
+count= text.count(substring)
+print(f"The substring '{substring}' occurs {count} times in text")

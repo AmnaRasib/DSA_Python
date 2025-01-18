aa0f5d5cfe1c96b5640d0298351b97e34d4c7462
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 19:00:54 +0500

    Create task15.py

---
+++ LAB_1/task15.py
@@ -0,0 +1,9 @@
+# Python Functions
+
+# Define a function to greet the user
+def greet(name):
+    print(f"Salam, {name}! Welcome to Python Programming ")
+#call the function
+greet("Aliyar")
+greet("Mehmet")    
+

d9433ed887553747ac94f2cfdb0a6456ee16a04b
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 12:12:54 +0500

    Create task6.py

---
+++ LAB_1/task6.py
@@ -0,0 +1,9 @@
+a=10
+b=20
+print("Is a is equal to b",a==b)
+print("Is a less than b",a<b)
+print("Is a Graeter than b",a>b)
+
+print("Logical AND: ",a<b and a>b)
+print("Logical OR: ",a<b or a>b)
+

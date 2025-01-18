9c710d32dd262f8e93552e4ab607a77aa0e28702
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 11:59:52 +0500

    Create task4.py

---
+++ LAB_1/task4.py
@@ -0,0 +1,9 @@
+integer_value= 10
+float_value=20.5
+string_value="Hellow world"
+boolean_value= True
+
+print(type (integer_value))
+print(type (float_value))
+print(type(string_value))
+print(type (boolean_value))

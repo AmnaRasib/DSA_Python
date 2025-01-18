57d3ff8e78dc4dca9364986ae593babfb75632eb
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 12:00:06 +0500

    Create task5.py

---
+++ LAB_1/task5.py
@@ -0,0 +1,8 @@
+text="Python Programming"
+print("First 6 characters: ",text[:6])
+print("Last 6 Characters: ",text[:-6])
+
+greeting="Hello,"
+print("Greeting: ",greeting+text)
+print("Uppercase: ",text.upper())
+print("Replace python with java",text.replace("Python","Java"))

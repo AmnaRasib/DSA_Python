f41192f7840f626c71ad70c346284690662df546
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 17:50:35 +0500

    Create task11.py

---
+++ LAB_1/task11.py
@@ -0,0 +1,7 @@
+# python dictionaries
+student={"name":"Ali","age":"21","grade":"A"}
+student["age"]=22
+print("Updated Dictionary: ",student)
+for key,values in student.items():
+    print(key,":",values)
+    

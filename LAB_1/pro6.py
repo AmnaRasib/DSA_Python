582db3cb4486d22a6913207e48aecfee5b1fb7e1
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-18 20:32:11 +0500

    Create pro6.py

---
+++ LAB_1/pro6.py
@@ -0,0 +1,5 @@
+#Convert Celsius to Fahrenheit
+temp_in_C=float(input("Enter Temperature(Celsius): "))
+print("Temperature in Celsius: ",temp_in_C,"deg C")
+fahrenheit= (temp_in_C*9/5)+32
+print("Temperature in Fahrenheit: ",fahrenheit,"F")

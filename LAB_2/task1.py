9aced8d647b94abbaff35cc470cd7e351087fb18
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-24 11:20:08 +0500

    Create task1.py

---
+++ LAB_2/task1.py
@@ -0,0 +1,10 @@
+# Python Arrays
+import array
+# an array of integers
+numbers= array.array('i',[1,2,3,4,5])
+print("Original Array: ",numbers)
+numbers[1]=10
+print("Modified Array: ",numbers)
+numbers.append(6)
+numbers.pop(0)
+print("Updated Array: ",numbers)

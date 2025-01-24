9e529770d4ed631381d1b04116399b42dcecf91e
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-24 11:43:31 +0500

    Update task1.py

--- LAB_2/task1.py
+++ LAB_2/task1.py
@@ -5,6 +5,7 @@ numbers= array.array('i',[1,2,3,4,5])
 print("Original Array: ",numbers)
 numbers[1]=10
 print("Modified Array: ",numbers)
+#  Add and remove elements
 numbers.append(6)
 numbers.pop(0)
 print("Updated Array: ",numbers)

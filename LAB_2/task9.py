1a7145bba123795aa447b0e8b1f66e0e466b110a
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-01-26 14:34:00 +0500

    Create task9.py

---
+++ LAB_2/task9.py
@@ -0,0 +1,7 @@
+import math_operations
+# Use a custom module
+print("Addition: ",math_operations.add(10,50))
+print("Multiplication: ",math_operations.multiply(10,50,10))
+# Use built-in module
+import math
+print("Squareroot of 4: ",math.sqrt(4))

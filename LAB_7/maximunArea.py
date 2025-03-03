36bea192b516cadf6a23e0329492df068d2cdb79
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-03-03 05:36:32 +0500

    Create maximunArea.py

---
+++ LAB_7/maximunArea.py
@@ -0,0 +1,20 @@
+def getMaxArea(arr):
+    res=0   #holds Maximum Area
+    n=len(arr) # Length of Array
+    for i in range (n):
+        curr=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>=arr[i]:
+            curr+=arr[i]
+            j-=1
+        j=i+1 
+        while j<n and arr[j]>=arr[i]:
+            curr+=arr[i]
+            j+=1
+        res= max(res,curr)
+    return res
+if __name__ == "__main__":
+    arr = [60, 20, 50, 40, 10, 50, 60]
+    print(getMaxArea(arr))
+
+

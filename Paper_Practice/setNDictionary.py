43c7dfaa010ce45b05ff9892c5f672ff8547f8c6
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-03-06 16:34:01 +0500

    Create setNDictionary.py

---
+++ Paper_Practice/setNDictionary.py
@@ -0,0 +1,12 @@
+my_set={1,2,3,4,5,6}
+print("original Set: ",my_set)
+my_set.add(7)
+my_set.add(8)
+my_set.remove(5)
+print("Updated Set: ",my_set)
+
+my_dict={"name": "Alice","age": "20"}
+print("original Dictionary: ",my_dict)
+my_dict["age"]=12
+my_dict["country"]="Pakistan"
+print("Updated Dictionary: ",my_dict)

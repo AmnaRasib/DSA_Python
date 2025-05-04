9e11b5a8fe211b26bee05c50882e279b37def35d
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-05-04 13:01:35 +0500

    Create MinInsertion.py

---
+++ Heap/MinInsertion.py
@@ -0,0 +1,24 @@
+class Minheap:
+    def __init__(self):
+        self.heap=[]
+    def insert(self,value):
+        self.heap.append(value)
+        current=len(self.heap)-1
+        while current >0: 
+            parent= (current-1) //2
+            if self.heap[current] < self.heap[parent]:
+                self.heap[current] , self.heap[parent] =self.heap[parent] ,self.heap[current]
+                current=parent
+            else:
+                break
+    def show_heap(self):
+        print("Heap:", self.heap)
+
+# Example
+heap = Minheap()
+heap.insert(10)
+heap.insert(5)
+heap.insert(3)
+heap.insert(2)
+heap.insert(8)
+heap.show_heap()

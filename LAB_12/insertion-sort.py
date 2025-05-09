63ad1e1eb9d4cc27d52a66e96f71ece257da23d0
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-05-09 23:51:41 +0500

    Update insertion-sort.py

--- LAB_12/insertion-sort.py
+++ LAB_12/insertion-sort.py
@@ -1,9 +1,13 @@
 def insertion_sort(arr):
     n=len(arr)
-    for i in range(n):
-        for j in range (i+1,n):
-           if arr[0]>arr[j]:
-             arr[0],arr[j]=arr[j],arr[0]
+    for i in range(1,n):
+        key=arr[i] 
+        j=i-1      
+        while j>=0 and arr[j]>key:
+          arr[j+1]=arr[j] 
+          j-=1
+        arr[j+1]=key
+        print(f"After pass {i}: {arr}")  
     return arr
 data = [5, 3, 8, 4, 2]
 insertion_sort(data)

fbf3e0a58f1e1197b22699b65e5ed87393ac3051
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-05-09 23:51:50 +0500

    Create task2.py

---
+++ LAB_12/intermediate_roblen/task2.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr)<=1:
+        return arr
+    n=len(arr)
+    pivot=arr[n-1]
+    left=[x for x in arr [:-1] if x < pivot]
+    right=[x for x in arr[:-1] if x>=pivot]
+    return quick_sort(left)+[pivot]+quick_sort(right)
+
+data = [5, 3, 8, 4, 2]
+sorted_data = quick_sort(data) 
+print("Sorted List:", sorted_data)
+    

3bf40eeeb4e66bdc6de551a04fa4b84925c6e437
Amna Rasib <23-CP-34@students.uettaxila.edu.pk>
2025-05-11 22:31:43 +0500

    Create tempCodeRunnerFile.py

---
+++ LAB-13/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+for node in graph:
+    print(f"{node} -> {graph[node]}")
